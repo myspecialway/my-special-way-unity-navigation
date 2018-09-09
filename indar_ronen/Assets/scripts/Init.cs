8ce1b41599c4b0fd74b46ad08f1c51a972f89069
Buslik, Raz (rb580s) <rb580s@intl.att.com>
2018-09-09 09:21:24 +0300

    Some script improvments

---
+++ indar_ronen/Assets/scripts/Init.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Init : MonoBehaviour
+{
+
+	[SerializeField]
+	private Navigation nav;
+
+	[SerializeField]
+	private string destName;
+	
+	private void Start ()
+	{
+		var dest = GameObject.Find(destName);
+
+		if (dest != null)
+		{
+			StartCoroutine(nav.CalculateNavAndShowPath(dest.transform));
+		}
+	}
+}
