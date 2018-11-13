2262016bf1ab4ccac79c7a99943616aa2149fbea
Avi Stone <avi.stone@gmail.com>
2018-11-13 11:09:50 +0200

    [unity] more cleanup. adding arrows prefabs

---
+++ Assets/scripts/SceneScreenNavigator.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneScreenNavigator : MonoBehaviour {
+
+    public string m_BackButtonNavigation = "[Name of Scene To Load]";
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            HandleBackButtonPressed();
+        }
+    }
+
+    public void HandleBackButtonPressed()
+    {
+        Debug.Log("HandleBackButtonPressed() called.");
+        if (SceneManager.GetActiveScene().name != m_BackButtonNavigation)
+            LoadScene(m_BackButtonNavigation);
+    }
+
+    void LoadScene(string sceneName)
+    {
+        Debug.Log("LoadScene(" + sceneName + ") called.");
+        if (!string.IsNullOrEmpty(sceneName))
+        {
+            SceneManager.LoadScene(sceneName);
+        } else {
+            Application.Quit();
+        }
+    }
+
+    public void LoadSceneZero()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LoadSceneOne()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
