911fea2c7bf1559725049fcf515c99f8ebd08164
Avi Stone <avi.stone@gmail.com>
2018-11-15 14:24:36 +0200

    [unity] add a path that is moving dynamically instead of plain line

---
+++ Assets/scripts/MoveX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveX : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.localPosition.x > 1f)
+        {
+            transform.localPosition = Vector3.zero;
+        }
+        else
+        {
+            transform.Translate(Time.deltaTime, 0, 0);
+        }
+    }
+}
---
+++ Assets/scripts/MoveY.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveY : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.localPosition.z < -1f)
+        {
+            transform.localPosition = Vector3.zero;
+        }
+        else
+        {
+            transform.Translate(0, 0, -Time.deltaTime);
+        }
+    }
+}
